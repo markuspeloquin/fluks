a5dd452a13acfe34854e29adf39bffed23370680
Markus Peloquin <markus@cs.wisc.edu>
2009-02-18 15:43:35 +0000

    Done for realsies.
    git-svn-id: https://fluks.googlecode.com/svn@11 6b01645e-0d02-11de-a42d-873b86d837ff

--- pbkdf2.cpp
+++ pbkdf2.cpp
@@ -7,26 +7,40 @@
 namespace luks {
 namespace {
 
-inline void	pbkdf2_f(enum hash_type, const uint8_t *, uint32_t,
+inline void	pbkdf2_f(Hmac_function *hmacfn, const uint8_t *, uint32_t,
 		    const uint8_t[SZ_SALT], uint32_t, uint32_t, uint8_t *);
 
-// function F() of part 3 of the PBKDF2 definition in PKCS #5 v2.0, and
-// described in pbkdf2() below
+// Function F() in part 3 of the PBKDF2 algorithm [RFC 2898]
 inline void
-pbkdf2_f(enum hash_type type, const uint8_t *in, uint32_t sz_in,
+pbkdf2_f(Hmac_function *hmacfn, const uint8_t *passwd, uint32_t sz_passwd,
     const uint8_t salt[SZ_SALT], uint32_t iterations, uint32_t index,
     uint8_t *result)
 {
-	std::tr1::shared_ptr<Hmac_function> hmacfn =
-	    Hmac_function::create(type);
+	// 3.
+	// As defined:
+	//
+	// P: password
+	// S: salt
+	// c: iteration count
+	// i: index [1,l]
+	//
+	// INT(i): 32-bit big-endian encoding of i
+	//
+	// U_1 = PRF(P, S . INT(i))
+	// U_j = PRF(P, U_{j-1})
+	// (as in some circles, '.' means concatenate)
+	//
+	// F(P, S, c, i) = XOR for j=[1,c] of { U_j }
+	//
+	// I instead use 0-indexing (index: [0,l), j: [0,c)).
 
 	uint8_t u[hmacfn->length()];
 
-	// I use indices starting at 0, but PBKDF2 starts at 1
+	// switch from 0-indexing to 1-indexing
 	index = htonl(index + 1);
 
-	// compute U_0 (PBKDF2's U_1)
-	hmacfn->init(in, sz_in);
+	// compute U_0
+	hmacfn->init(passwd, sz_passwd);
 	hmacfn->add(salt, SZ_SALT);
 	hmacfn->add(reinterpret_cast<uint8_t *>(&index), 4);
 	hmacfn->end(u, sizeof(u));
@@ -35,7 +49,7 @@ pbkdf2_f(enum hash_type type, const uint8_t *in, uint32_t sz_in,
 
 	for (uint32_t i = 1; i < iterations; i++) {
 		// compute U_i
-		hmacfn->init(in, sz_in);
+		hmacfn->init(passwd, sz_passwd);
 		hmacfn->add(u, sizeof(u));
 		hmacfn->end(u, sizeof(u));
 
@@ -60,42 +74,34 @@ luks::pbkdf2(enum hash_type type, const uint8_t *in, uint32_t sz_in,
 		    "PBKDF2 derived key too long");
 
 	// 2.
+	// In the spec:
+	//
 	// l = ceil(sz_key / hash_size(type))
 	//	(number of blocks, including final partial block)
 	// r = dkLen - (l - 1) * hLen
 	//	(bytes in final partial block)
 	//
-	// I define blocks and partial differently.
+	// I define blocks and partial in a more useful way.
 	uint32_t blocks = sz_key / sz_hash;
 	uint32_t partial = sz_key % sz_hash;
 
 	// 3.
-	// (note that I start indices at 0 instead of 1)
-	// P: password
-	// S: salt
-	// c: iteration count
-	// INT(i): 32-bit big-endian encoding of i
-	// U_j defined on j=[0,c):
-	//	U_0 = PRF(P, S . INT(i))
-	//	U_j = PRF(P, U_{j-1})
-	//	(PRF: underlying pseudo-random function)
-	//	(as in some circles, '.' means concatenate)
-	// F(P, S, c, i) = XOR for j=[0,c) of { U_j }
-	// T_i defined on i=[0,l):
-	//	F(P, S, c, i)
+	// T_i = F(P, S, c, i) for i in [1,l]
 	//
-	// I combine this step with (4).
+	// I instead use 0-indexing.  I combine this step with (4).
 
 	// 4.
 	// Concatenate all T_i.  The first dkLen bytes is the derived key.
-	// Iterate over all but the partial block
+	// Iterate over all but the partial block.
+	std::tr1::shared_ptr<Hmac_function> hmacfn =
+	    Hmac_function::create(type);
 	for (uint32_t i = 0; i < blocks; i++)
-		pbkdf2_f(type, in, sz_in, salt, iterations, i,
+		pbkdf2_f(hmacfn.get(), in, sz_in, salt, iterations, i,
 		    derived_key + i * sz_hash);
 
 	if (partial) {
 		uint8_t buf_partial[sz_hash];
-		pbkdf2_f(type, in, sz_in, salt, iterations, blocks,
+		pbkdf2_f(hmacfn.get(), in, sz_in, salt, iterations, blocks,
 		    buf_partial);
 		memcpy(derived_key + blocks * sz_hash, buf_partial, partial);
 	}
--- pbkdf2.hpp
+++ pbkdf2.hpp
@@ -11,7 +11,7 @@
 namespace luks {
 
 void	pbkdf2(enum hash_type, const uint8_t *, uint32_t,
-	    const uint8_t[SZ_SALT], uint32_t, uint8_t *, uint32_t)
+	    const uint8_t[SZ_SALT], uint32_t, uint8_t *, uint32_t);
 
 }
 
