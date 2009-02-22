30a5daa47ae75790971966821c198a5805369c51
Markus Peloquin <markus@cs.wisc.edu>
2009-02-22 09:58:22 +0000

    Fix something for Doxygen.
    git-svn-id: https://fluks.googlecode.com/svn@33 6b01645e-0d02-11de-a42d-873b86d837ff

--- af.hpp
+++ af.hpp
@@ -15,12 +15,12 @@ namespace luks {
  * \param[in] in	The data to expand.
  * \param[in] sz	The size of the <code>in</code> in bytes.
  * \param[in] stripes	The number of strips to expand the data to.
- * \param[in] hash_type	The type of hash to use.
+ * \param[in] type	The type of hash to use.
  * \param[out] out	The output buffer, which is assumed to be of size
  *	<code>(sz * stripes)</code> bytes.
  */
-void	af_split(const uint8_t *in, size_t sz, size_t stripes, enum hash_type,
-	    uint8_t *out);
+void	af_split(const uint8_t *in, size_t sz, size_t stripes,
+	    enum hash_type type, uint8_t *out);
 
 /** Anti-forensic merge.
  *
@@ -30,11 +30,11 @@ void	af_split(const uint8_t *in, size_t sz, size_t stripes, enum hash_type,
  *	<code>(sz * stripes)</code> bytes.
  * \param[in] sz	The size of <code>out</code> in bytes.
  * \param[in] stripes	The number of strips to expand the data to.
- * \param[in] hash_type	The type of hash to use.
+ * \param[in] type	The type of hash to use.
  * \param[out] out	The output buffer.
  */
-void	af_merge(const uint8_t *in, size_t sz, size_t stripes, enum hash_type,
-	    uint8_t *out);
+void	af_merge(const uint8_t *in, size_t sz, size_t stripes,
+	    enum hash_type type, uint8_t *out);
 
 }
 
