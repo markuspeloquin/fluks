2891b6f45647b8e900a69eac80a95bc6323d5483
Markus Peloquin <markus@cs.wisc.edu>
2009-03-09 23:53:52 +0000

    dm.hpp documentation
    git-svn-id: https://fluks.googlecode.com/svn@85 6b01645e-0d02-11de-a42d-873b86d837ff

--- dm.hpp
+++ dm.hpp
@@ -10,8 +10,25 @@
 
 namespace luks {
 
-void	dm_close(const std::string &name);
+/** Remove the mapping for a DM device
+ *
+ * \param name		The name of the mapping
+ * \throw Dm_error	An error occurred
+ */
+void	dm_close(const std::string &name) throw (Dm_error);
 
+/** Add a mapping for a DM device
+ *
+ * \param name		The name for the mapping
+ * \param start_sector	The start sector of the encrypted data
+ * \param num_sectors	The size of the disk minus the start sector
+ * \param cipher_spec	A cipher spec to send to dm-crypt (see Linux dm-crypt
+ *	documentation)
+ * \param key		The master key
+ * \param sz_key	The size of the key in bytes
+ * \param device_path	The pathname of the device
+ * \throw Dm_error	An error occurred
+ */
 void	dm_open(const std::string &name, uint64_t start_sector,
 	    uint64_t num_sectors, const std::string &cipher_spec,
 	    const uint8_t *key, size_t sz_key, const std::string &device_path)
