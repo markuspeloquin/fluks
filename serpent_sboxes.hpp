bbe0345cdb901831211b1552c9462624e42741e1
Markus Peloquin <markus@cs.wisc.edu>
2009-04-16 23:51:42 +0000

    7/16 sboxes completed.
    git-svn-id: https://fluks.googlecode.com/svn@146 6b01645e-0d02-11de-a42d-873b86d837ff

--- serpent_sboxes.hpp
+++ serpent_sboxes.hpp
@@ -66,11 +66,30 @@ sbox_1_inv(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
 {
 }
 
-// S2:    8 6 7 9 3 c a f d 1 e 4 0 b 5 2
+// S2:    [8 6 7 9 3 c a f d 1 e 4 0 b 5 2] in 17 gates
 inline void
 sbox_2(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11,
+	    t12;
+	t0 = x0 ^ x1;
+	t1 = x3 ^ t0;
+	t2 = x0 | x2;
+	y0 = t1 ^ t2;
+	t3 = x0 ^ x2;
+	t4 = x2 ^ y0;
+	t5 = x1 & t4;
+	t6 = t3 ^ t5;
+	y3 = ~t6;
+	t7 = x1 ^ t4;
+	t8 = t2 & t7;
+	t9 = t0 | t6;
+	y1 = t8 ^ t9;
+	t10 = x0 ^ t4;
+	t11 = x2 ^ y1;
+	t12 = t9 & t10;
+	y2 = t11 ^ t12;
 }
 
 // Sinv2: c 9 f 4 b e 1 2 0 3 6 d 5 8 a 7
@@ -80,25 +99,80 @@ sbox_2_inv(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
 {
 }
 
-// S3:    0 f b 8 c 9 6 3 d 1 2 4 a 7 5 e
+// S3:    [0 f b 8 c 9 6 3 d 1 2 4 a 7 5 e] in 18 gates
 inline void
 sbox_3(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11,
+	    t12, t13;
+	t0 = x0 ^ x1;
+	t1 = x2 ^ x3;
+	t2 = x1 & t1;
+	t3 = x3 & t1;
+	t4 = t0 | t3;
+	y0 = t2 ^ t4;
+	t5 = x0 ^ x2;
+	t6 = x1 & t5;
+	t7 = t1 ^ t6;
+	t8 = x0 | t2;
+	y2 = t7 ^ t8;
+	t9 = x1 ^ t5;
+	t10 = x0 | t3;
+	t11 = t7 & t10;
+	y3 = t9 ^ t11;
+	t12 = x1 ^ t10;
+	t13 = y3 | t12;
+	y1 = t7 ^ t13;
 }
 
-// Sinv3: 0 9 a 7 b e 6 d 3 5 c 2 4 8 f 1
+// Sinv3: [0 9 a 7 b e 6 d 3 5 c 2 4 8 f 1] in 17 gates
 inline void
 sbox_3_inv(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11,
+	    t12;
+	t0 = x1 ^ x2;
+	t1 = x1 & t0;
+	t2 = x0 ^ t1;
+	t3 = x3 | t2;
+	y0 = t0 ^ t3;
+	t4 = x1 ^ x3;
+	t5 = t2 ^ y0;
+	t6 = t3 & t5;
+	y2 = t4 ^ t6;
+	t7 = x3 ^ t3;
+	t8 = x2 | t2;
+	t9 = t5 & t8;
+	y3 = t7 | t9;
+	t10 = x1 ^ y2;
+	t11 = t8 ^ t9;
+	t12 = y0 | t10;
+	y1 = t11 ^ t12;
 }
 
-// S4:    1 f 8 3 c 0 b 6 2 5 4 a 9 e 7 d
+// S4:    [1 f 8 3 c 0 b 6 2 5 4 a 9 e 7 d] in 15 gates
 inline void
 sbox_4(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
+	t0 = x0 ^ x3;
+	t1 = x3 & t0;
+	t2 = x2 ^ t1;
+	t3 = x1 | t2;
+	y3 = t0 ^ t3;
+	t4 = x1 & y3;
+	t5 = t2 ^ t4;
+	y0 = ~t5;
+	t6 = x1 ^ t0;
+	t7 = x3 ^ t2;
+	t8 = t5 | t7;
+	y2 = t6 ^ t8;
+	t9 = x0 ^ t2;
+	t10 = t8 & y2;
+	y1 = t9 ^ t10;
 }
 
 // Sinv4: 5 0 8 3 a 9 7 e 2 c b 6 4 f d 1
@@ -108,18 +182,57 @@ sbox_4_inv(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
 {
 }
 
-// S5:    f 5 2 b 4 a 9 c 0 3 e 8 d 6 7 1
+// S5:    [f 5 2 b 4 a 9 c 0 3 e 8 d 6 7 1] in 17 gates
 inline void
 sbox_5(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11,
+	    t12;
+	t0 = x0 ^ x1;
+	t1 = x0 ^ x2;
+	t2 = x0 ^ x3;
+	t3 = t0 | t2;
+	t4 = t1 ^ t3;
+	y0 = ~t4;
+	t5 = x1 ^ t2;
+	t6 = x3 | y0;
+	y1 = t5 ^ t6;
+	t7 = x3 ^ t6;
+	t8 = x1 | t4;
+	t9 = t5 | t7;
+	y2 = t8 ^ t9;
+	t10 = x0 ^ t4;
+	t11 = x3 ^ y2;
+	t12 = t5 | t11;
+	y3 = t10 ^ t12;
 }
 
-// Sinv5: 8 f 2 9 4 1 d e b 6 5 3 7 c a 0
+// Sinv5: [8 f 2 9 4 1 d e b 6 5 3 7 c a 0] in 18 gates
 inline void
 sbox_5_inv(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
     uint32_t &y0, uint32_t &y1, uint32_t &y2, uint32_t &y3)
 {
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11,
+	    t12, t13;
+	t0 = x0 ^ x3;
+	t1 = x0 & x1;
+	t2 = x1 & x2;
+	t3 = t0 | t1;
+	y0 = t2 ^ t3;
+	t4 = x0 ^ x2;
+	t5 = x0 & x3;
+	t6 = x1 & t3;
+	t7 = t4 | t5;
+	y2 = t6 ^ t7;
+	t8 = x1 ^ x2;
+	t9 = ~t8;
+	t10 = t1 | t9;
+	y3 = t5 ^ t10;
+	t11 = x0 ^ x1;
+	t12 = t3 ^ t11;
+	t13 = t9 | t11;
+	y1 = t12 ^ t13;
 }
 
 // S6:    7 2 c 5 8 4 6 b e 9 1 f d 3 a 0
