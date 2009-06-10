edf16f2c4e9678bcd87be144ae083e6bf4d440b1
Markus Peloquin <markus@cs.wisc.edu>
2009-06-09 23:30:16 -0700

    Corrected compute_last_d(). 2 bugs.

--- af.cpp
+++ af.cpp
@@ -26,47 +26,49 @@
 namespace fluks {
 namespace {
 
-void	compute_d(const uint8_t *, size_t, unsigned, enum hash_type,
+void	compute_last_d(const uint8_t *, size_t, unsigned, enum hash_type,
 	    uint8_t *);
 void	hash1(const uint8_t *, size_t, enum hash_type, uint8_t *);
 void	hash2(const uint8_t *, size_t, enum hash_type, uint8_t *);
 
-// compute the last d_i value
+// compute d_{n-1}: the last d_i value
 void
-compute_d(const uint8_t *s, size_t sz, unsigned stripes,
-    enum hash_type type, uint8_t *final_d)
+compute_last_d(const uint8_t *s, size_t sz, unsigned stripes,
+    enum hash_type type, uint8_t *d_f)
 {
-	uint8_t d[sz];
+	uint8_t		d[sz];
+	uint8_t		*d_0; // d_{i+0}
+	uint8_t		*d_1; // d_{i+1}
 
-	uint8_t *d_odd;
-	uint8_t *d_even;
+	Assert(stripes, "'stripes' must be positive");
 
-	// last to compute is d_{stripes-1}, which should be stored
-	// at final_d
-	
+	// last to compute is d_{stripes-1}, which should be stored at d_f
+
 	if (stripes % 2) {
 		// (stripes - 1) is even
-		d_odd = d;
-		d_even = final_d;
+		d_0 = d_f;
+		d_1 = d;
 	} else {
-		d_odd = final_d;
-		d_even = d;
+		d_0 = d;
+		d_1 = d_f;
 	}
 
-	std::fill(d_even, d_even + sz, 0);
+	std::fill(d_0, d_0 + sz, 0);
 
-	for (unsigned i = 1; i < stripes - 1; i++) {
-		// d_even ^= i'th stripe
-		xor_bufs(d_even, s + i * sz, sz, d_even);
-		// d_odd = H(d_even)
-		hash1(d_even, sz, type, d_odd);
+	for (unsigned i = 1; i < stripes; i++) {
+		// d_0 ^= i'th stripe
+		xor_bufs(d_0, s, sz, d_0);
+		// d_1 = H(d_0)
+		hash1(d_0, sz, type, d_1);
+		s += sz;
 
-		if (++i == stripes - 1) return;
+		if (++i == stripes) return;
 
-		// d_odd ^= i'th stripe
-		xor_bufs(d_odd, s + i * sz, sz, d_odd);
-		// d_even = H(d_odd)
-		hash1(d_odd, sz, type, d_even);
+		// d_1 ^= i'th stripe
+		xor_bufs(d_1, s, sz, d_1);
+		// d_0 = H(d_1)
+		hash1(d_1, sz, type, d_0);
+		s += sz;
 	}
 	return;
 }
@@ -176,7 +178,7 @@ fluks::af_split(const uint8_t *in, size_t sz, size_t stripes,
 	// d_0 = 0
 	// d_k = H( d_{k-1} ^ s_k )
 	// s_n = d_{n-1} ^ D
-	compute_d(out, sz, stripes, type, d);
+	compute_last_d(out, sz, stripes, type, d);
 	xor_bufs(d, in, sz, out + sz * (stripes - 1));
 }
 
@@ -191,6 +193,6 @@ fluks::af_merge(const uint8_t *in, size_t sz, size_t stripes,
 	// d_0 = 0
 	// d_k = H( d_{k-1} ^ s_k )
 	// D = d_{n-1} ^ s_n
-	compute_d(in, sz, stripes, type, d);
+	compute_last_d(in, sz, stripes, type, d);
 	xor_bufs(d, in + sz * (stripes - 1), sz, out);
 }
