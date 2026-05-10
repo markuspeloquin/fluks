947cc191a2637438b081d559a3411ba1a58504c9
Markus Peloquin <markus@cs.wisc.edu>
2026-05-10 15:15:53 -0700

    Move license info to LICENSE

--- whirlpool.c
+++ whirlpool.c
@@ -1,20 +1,3 @@
-/* Copyright (c) 2009, Markus Peloquin <markus@cs.wisc.edu>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
- * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
-
-/* I changed so much damn stuff in here that I feel I can copyright it. I had
- * nothing to do with the c_# tables or the process_buffer algorithm. */
-
 /**
  * The Whirlpool hashing function.
  *
